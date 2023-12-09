a1cdd592ba8adeeda86bce8a84765a3718f78a01
Jyothi-Gaddameedi <g.jyothi.779@gmail.com>
2023-12-09 17:17:19 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1> This line added by Kalyani </h1> 
+      <h2>This line added by Jyothi</h2>
     </div>
   );
 }
