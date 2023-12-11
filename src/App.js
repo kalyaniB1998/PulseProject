8e31e6e85af7b539860d0a3a5d035fff33b44809
kalyani890 <153181208+kalyani890@users.noreply.github.com>
2023-12-11 13:13:18 -0800

    added signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
       <Login></Login>
-      <h1> This line added by Kalyani </h1> 
+   <h1> This line added by Kalyani </h1> 
     </div>
   );
 }
