e7dbe68855fe5281f597433757b8c5f880b31a23
Kalyani <kalyaniambhore98@gmail.com>
2023-12-11 15:51:31 +0530

    added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-      <h1> This line added by Kalyani </h1> 
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/home' element={<Home/>}></Route>
+      
+    </Routes>
+
+    </BrowserRouter>
+   
   );
 }
 
