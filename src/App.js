83ab2bb6f644b5e7f98e7b86b38590893fa8247f
Jyothi-Gaddameedi <g.jyothi.779@gmail.com>
2023-12-11 15:15:18 +0530

    added TopNavigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Home from './components/Home';
+import Signup from './components/Signup';
 
 function App() {
   return (
