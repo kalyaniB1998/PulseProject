bf67f22e086366194c2b26aecb4ff0c544ede2ff
AbhishaktaG <gudipudimounika2002@gmail.com>
2023-12-11 16:21:43 +0530

    added signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+
+
 
 function App() {
   return (
@@ -10,8 +14,7 @@ function App() {
         <Route path="/" element={<Login/>}></Route>
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
-        <Route path="/">Logout</Route>
-      </Routes>
+       </Routes>
       </BrowserRouter>
     </div>
   );
