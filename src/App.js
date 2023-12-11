83dda827a4f0cf7ba08d2fd3263df2289f81048f
Jyothi-Gaddameedi <g.jyothi.779@gmail.com>
2023-12-11 13:02:36 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
+      <Login></Login>
       <h1> This line added by Kalyani </h1> 
       <h2>This line added by Jyothi</h2>
     </div>
