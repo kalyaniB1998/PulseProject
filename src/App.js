6dbc6415c22d18db49beb33dafb1314206c082a5
Kalyani <kalyaniambhore98@gmail.com>
2023-12-11 15:27:24 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+
 import Login from './components/Login';
 import Home from './components/Home';
 import Signup from './components/Signup';
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Login></Login>
+      
       <h1> This line added by Kalyani </h1> 
     </div>
   );
