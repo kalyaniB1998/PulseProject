77835f013d571412ef0e902dd1cb98f35266e36a
Jyothi-Gaddameedi <g.jyothi.779@gmail.com>
2023-12-14 12:26:00 +0530

    added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,10 @@ import TopNavigation from './TopNavigation'
 function Home() {
   return (
     <div className='App'>
-           <TopNavigation/>
+    <TopNavigation/>
         <img src='./images/brn.png' className='img'></img>
         <h1>Welcome To Home Page</h1>
-
-  
-       
-
-
+        
     </div>
   )
 }
